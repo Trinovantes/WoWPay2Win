201a96918ac67f56b48592c0d20c91d31e0ff80d
Stephen Li <stephenli.ca@gmail.com>
2021-02-04 17:51:07 -0500

    Break out of fetchRegion loop during dev mode

--- src/cron/utils/fetchRegions.ts
+++ src/cron/utils/fetchRegions.ts
@@ -4,15 +4,14 @@ import { Region } from '@cron/models/Region'
 export default async function fetchRegions(): Promise<Array<Region>> {
     const regions: Array<Region> = []
 
-    if (DEFINE.IS_DEV) {
-        const region = new Region(RegionConfigs[RegionConfigs.length - 1])
+    for (const regionConfig of RegionConfigs) {
+        const region = new Region(regionConfig)
         await region.fetch()
         regions.push(region)
-    } else {
-        for (const regionConfig of RegionConfigs) {
-            const region = new Region(regionConfig)
-            await region.fetch()
-            regions.push(region)
+
+        // Speed up runs during development
+        if (DEFINE.IS_DEV) {
+            break
         }
     }
 
