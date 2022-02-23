f29fa54b0978eb7c50fb14bdf7c6671c22738eae
Stephen Li <stephenli.ca@gmail.com>
2022-02-23 17:57:33 -0500

    Comment useFilterSyncLocalStorage

--- src/web/store/Filter/useFilterSyncLocalStorage.ts
+++ src/web/store/Filter/useFilterSyncLocalStorage.ts
@@ -3,16 +3,17 @@ import { useFilterStore } from '.'
 import { HydrationKey, loadStateFromLocalStorage, saveStateToLocalStorage } from '../Hydration'
 
 export function useFilterSyncLocalStorage() {
-    const filterStore = useFilterStore()
     const router = useRouter()
+    const filterStore = useFilterStore()
 
+    // Load state from localStorage if there's no init query
     const savedState = loadStateFromLocalStorage(HydrationKey.FILTER)
     const hasNoInitQuery = Object.keys(router.currentRoute.value.query).length === 0
-
     if (savedState && hasNoInitQuery) {
         filterStore.$patch(savedState)
     }
 
+    // Write changes to localStorage
     filterStore.$subscribe((mutation, state) => {
         saveStateToLocalStorage(HydrationKey.FILTER, state)
     })
