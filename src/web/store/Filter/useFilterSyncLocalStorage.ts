839e8537d395a6b9ee8efe68ceadb202e0d673da
Stephen Li <stephenli.ca@gmail.com>
2022-02-23 15:54:22 -0500

    Skip localStorage hydration if route has query

--- src/web/store/Filter/useFilterSyncLocalStorage.ts
+++ src/web/store/Filter/useFilterSyncLocalStorage.ts
@@ -1,11 +1,15 @@
+import { useRouter } from 'vue-router'
 import { useFilterStore } from '.'
 import { HydrationKey, loadStateFromLocalStorage, saveStateToLocalStorage } from '../Hydration'
 
 export function useFilterSyncLocalStorage() {
     const filterStore = useFilterStore()
+    const router = useRouter()
 
     const savedState = loadStateFromLocalStorage(HydrationKey.FILTER)
-    if (savedState) {
+    const hasNoInitQuery = Object.keys(router.currentRoute.value.query).length === 0
+
+    if (savedState && hasNoInitQuery) {
         filterStore.$patch(savedState)
     }
 
