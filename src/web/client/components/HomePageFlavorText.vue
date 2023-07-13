05d377ef1c93a10d6eaa2818688ff5dd8756b9b7
Stephen Li <stephenli.ca@gmail.com>
2023-07-12 20:49:53 -0400

    Update flavor text

--- src/web/client/components/HomePageFlavorText.vue
+++ src/web/client/components/HomePageFlavorText.vue
@@ -3,22 +3,25 @@ import { onMounted, ref } from 'vue'
 
 const DAYS_OF_WEEK = 7
 const daysSinceTues = (DAYS_OF_WEEK + (new Date().getDay() - 2)) % DAYS_OF_WEEK
-const nonClassicExpansions = ['WotLK', 'Cataclysm', 'MoP', 'WoD', 'Legion', 'BfA']
+const nonClassicExpansions = ['Cataclysm', 'MoP', 'WoD', 'Legion', 'BfA', 'Shadowlands']
 const memes = [
     ...nonClassicExpansions.map((expac) => `${expac} Classic waiting room`),
     'We are trying to reach you about your mount\'s extended warranty.',
     'Does anybody else think we should delete pandas from the game?',
     'Sometimes I wonder how much money I would make if I had put ads on this site',
-    'Imagine if I got a commission everytime somebody made a purchase because of this website',
     'Isn\'t this just pay2win with extra steps?',
     'I bet there are still realms that have Realm First Kil\'Jaeden available',
     'I\'m calling the overtime police',
     'Anyone caught playing Classic will be gkicked',
     'Please buff BDK',
     'Sponsored by Raid: Shadow Lands',
-    'Friendship with WoW ended; Runescape is my new best friend',
     `${daysSinceTues} ${daysSinceTues === 1 ? 'day' : 'days'} since last wipe on Shriekwing`,
+    `${daysSinceTues} ${daysSinceTues === 1 ? 'day' : 'days'} since last wipe on Zskarn`,
     'Next expansion will be different',
+    'A triumphant roar echoes from atop the Seat of the Aspects as Nasz\'uro, the Unbound Legacy, is formed.',
+    'A twiumphant woaw echoes fwom atop the Seat of the Aspects as Nyasz\'uwu, the Unbound Wegacy is fowmed >:3',
+    'Remember: exploit early, exploit often',
+    'Huffs Copium',
 ]
 
 const flavorText = ref<string | undefined>('')
@@ -29,8 +32,7 @@ onMounted(() => {
 
 <template>
     <footer>
-        <!-- eslint-disable-next-line vue/no-v-html -->
-        <p class="flavor-text" v-html="flavorText" />
+        <p class="flavor-text" v-text="flavorText" />
 
         <p class="links">
             <a href="https://github.com/Trinovantes/WoWPay2Win">GitHub</a>
