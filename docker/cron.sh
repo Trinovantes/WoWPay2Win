fae8e87cbff73a3340fd9610989b93e00cbaab1d
Stephen Li <stephenli.ca@gmail.com>
2024-01-21 23:54:40 -0500

    Disable sentry cron monitor

--- docker/cron.sh
+++ docker/cron.sh
@@ -3,10 +3,10 @@
 set -e # Exit on error
 set -u # Error when undefined variable
 
-SENTRY_CRONS="https://o504161.ingest.sentry.io/api/5590531/cron/fetch-auctions/ba1fbe840db046beb61f9ae59e888924/"
+# SENTRY_CRONS="https://o504161.ingest.sentry.io/api/5590531/cron/fetch-auctions/ba1fbe840db046beb61f9ae59e888924/"
 
-curl "${SENTRY_CRONS}?status=in_progress"
+# curl "${SENTRY_CRONS}?status=in_progress"
 
 yarn fetchAuctions
 
-curl "${SENTRY_CRONS}?status=ok"
+# curl "${SENTRY_CRONS}?status=ok"
