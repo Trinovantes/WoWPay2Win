a06a759e037273322c4dc2c934dda5648e180c28
Stephen Li <stephenli.ca@gmail.com>
2024-09-11 01:15:14 -0400

    Add t38 Nerub-ar Palace

--- src/common/TierConfig.ts
+++ src/common/TierConfig.ts
@@ -31,10 +31,11 @@ export enum Tier {
 
     // War Within
     WarWithinProfessions = 'war-within-professions',
+    NerubArPalace = 'nerub-ar-palace',
 }
 
 // IMPORTANT: Update every tier
-export const DEFAULT_TIER = Tier.WarWithinProfessions
+export const DEFAULT_TIER = Tier.NerubArPalace
 
 export type BoeGearLabel = 'Plate' | 'Mail' | 'Leather' | 'Cloth' | 'Weapon' | 'Back' | 'Jewelry' | 'Trinket' | 'Raid BoEs'
 export type BoeProfessionLabel = 'Jewelcrafting' | 'Blacksmithing' | 'Leatherworking' | 'Tailoring' | 'Inscription' | 'Alchemy' | 'Enchanting' | 'Engineering' | 'Cooking'
@@ -59,6 +60,51 @@ export type TierConfig = Readonly<{
 }>
 
 export const TIER_CONFIGS: Readonly<Record<Tier, TierConfig>> = {
+    [Tier.NerubArPalace]: {
+        name: 'Nerub-ar Palace',
+        iconPath: '38-nerub-ar-palace.png',
+        boes: [
+            {
+                label: 'Plate',
+                ids: [
+                    225727,
+                    225744,
+                ],
+            },
+            {
+                label: 'Mail',
+                ids: [
+                    225724,
+                    225725,
+                ],
+            },
+            {
+                label: 'Leather',
+                ids: [
+                    225722,
+                    225723,
+                ],
+            },
+            {
+                label: 'Cloth',
+                ids: [
+                    225720,
+                    225721,
+                ],
+            },
+            {
+                label: 'Jewelry',
+                ids: [
+                    225728,
+                ],
+            },
+        ],
+        ilvlStep: 13,
+        ilvlRange: {
+            min: 590,
+            max: 590 + (13 * 3) + 9, // Later bosses drop up to base+9
+        },
+    },
     [Tier.WarWithinProfessions]: {
         name: 'War Within Profession Recipes',
         iconPath: '0-war-within.png',
@@ -141,7 +187,6 @@ export const TIER_CONFIGS: Readonly<Record<Tier, TierConfig>> = {
             },
         ],
     },
-
     [Tier.DragonflightAwakened]: {
         name: 'Dragonflight Awakened Raids',
         iconPath: '0-dragonflight-awakened.jpg',
