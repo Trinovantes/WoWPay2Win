1f35f49a4facdb772396b531bb56cdc5545468af
Stephen Li <stephenli.ca@gmail.com>
2024-09-26 18:01:38 -0400

    Upgrade dependencies

--- build/webpack.config.web.ts
+++ build/webpack.config.web.ts
@@ -53,8 +53,8 @@ export default merge(commonConfig, {
                         options: {
                             additionalData: (content: string, loaderContext: { resourcePath: string }): string => {
                                 return (loaderContext.resourcePath.endsWith('sass'))
-                                    ? '@use "sass:math"\n @import "@/web/client/assets/css/variables.scss"\n' + content
-                                    : '@use "sass:math";  @import "@/web/client/assets/css/variables.scss"; ' + content
+                                    ? '@use "sass:color"\n@use "sass:math"\n@import "@/web/client/assets/css/variables.scss"\n' + content
+                                    : '@use "sass:color"; @use "sass:math"; @import "@/web/client/assets/css/variables.scss"; ' + content
                             },
                         },
                     },
